35493ff19efbadaaf88d04d885a77f6c319a0c65
frontback333 <gunhu2007@gmail.com>
2026-03-31 14:28:50 +0900

    little changes

--- 실습/calender.py
+++ 실습/calender.py
@@ -1,14 +1,14 @@
-date_quantity=[31,28,31,30,31,30,31,31,30,31,30,31]
-month=int(input())
-startdate=4
+date_quantity = [31,28,31,30,31,30,31,31,30,31,30,31]
+month = int(input())
+startdate = 4
 for i in range(month-1):
-    startdate+=date_quantity[i]
-startdate%=7
+    startdate += date_quantity[i]
+startdate %= 7
 print(f"        {month}월")
 print("일 월 화 수 목 금 토")
 for i in range(startdate):
     print("   ",end='')
 for i in range(date_quantity[month+1]):
-    if (startdate+i)%7==0:
+    if (startdate + i)%7==0:
         print()
     print(f"{i+1:2d}",'',end='')
